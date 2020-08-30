28836e9d13e7f762d5617029a08a7aefb4ccde2e
= <=>
2020-08-30 10:13:25 -0300

    Adicionando testes nas rotas

--- ch03/lib/handlers.js
+++ ch03/lib/handlers.js
@@ -1,9 +1,9 @@
 const fortune = require('./fortune');
 
-exports.home = ({ req, res }) => res.render('home');
+exports.home = ( req, res ) => res.render('home');
 
-exports.about = ({ req, res }) => res.render('about', { fortune: fortune.getFortune() });
+exports.about = ( req, res ) => res.render('about', { fortune: fortune.getFortune() });
 
 exports.notFound = (req, res) => res.render('404');
 
-exports.serverError = (err, req, res, next) => res.render('500');
+exports.serverError = (req, res, err, next) => res.render('500');
